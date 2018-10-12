a274c2d1fc61f35a588240e205e9cad3665b1635
Surekha Srinivasan <surekhasrao@gmail.com>
2018-10-12 10:14:21 +0000

    Print a positive integer using main function

---
+++ positive.py
@@ -0,0 +1,16 @@
+# Print a positive integer
+from cs50 import get_int
+
+def main():
+    i = get_positive_int("Positive integer, please: ")
+    print(i)
+
+def get_positive_int(prompt):
+    while True:
+        n = get_int(prompt)
+        if n >= 1:
+            break
+    return n
+
+if __name__ == "__main__":
+    main()
